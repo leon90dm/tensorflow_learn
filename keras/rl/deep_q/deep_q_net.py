40d2c37645896852baf6bf894beca69b4540e09e
mao.dai <mao.dai@wormpex.com>
2018-09-30 16:44:16 +0800

    deep_q_network

--- keras/rl/deep_q/deep_q_net.py
+++ keras/rl/deep_q/deep_q_net.py
@@ -21,14 +21,14 @@ class DeepQNetwork:
 
         self.n_actions = n_actions
         self.n_features = n_features
-        self.memory = np.zeros([memory_size, n_features * 2 + 2])
+        self.memory = np.zeros([memory_size, n_features * 2 + 3])
         self.memory_size = memory_size
         self.memory_offset = 0
         self.hidden_size = hidden_size
         self.learn_step = 0
 
-    def save_transition(self, s, a, r, s_):
-        stack = np.hstack([s, [a, r], s_])
+    def save_transition(self, s, a, r, flag, s_):
+        stack = np.hstack([s, [a, r, flag], s_])
         self.memory[self.memory_offset % self.memory_size, :] = stack
         self.memory_offset += 1
 
@@ -43,7 +43,9 @@ class DeepQNetwork:
             keras.layers.Dense(self.hidden_size, input_shape=[self.n_features]),
             keras.layers.Dense(self.n_actions)
         ])
-        self.eval_model.compile(optimizer='RMSprop', loss='mse')
+        rms = keras.optimizers.RMSprop(lr=0.01)
+        loss = keras.losses.mse
+        self.eval_model.compile(optimizer=rms, loss=loss)
         print(self.eval_model.summary())
 
     def choose_action(self, observation):
@@ -71,8 +73,9 @@ class DeepQNetwork:
         q_target = q_eval.copy()
 
         reward = batch_memory[:, self.n_features + 1]
+        flags = batch_memory[:, self.n_features + 2]
         eval_act_index = batch_memory[:, self.n_features].astype(int)
-        q_target[np.arange(len(batch_memory)), eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
+        q_target[np.arange(len(batch_memory)), eval_act_index] = reward + self.gamma * np.max(q_next, axis=1) * flags
         loss = self.eval_model.train_on_batch(batch_memory[:, : self.n_features], y=q_target)
 
         # increasing epsilon
@@ -80,9 +83,11 @@ class DeepQNetwork:
         self.learn_step += 1
         return loss
 
+
 env = Maze()
 deep_q = DeepQNetwork(n_actions=env.n_actions,
                       n_features=env.n_features,
+                      batch_size=200,
                       memory_size=2000)
 deep_q.build_net()
 
@@ -103,11 +108,11 @@ def run_maze():
             # RL take action and get next observation and reward
             observation_, reward, done = env.step(action)
 
-            deep_q.save_transition(observation, action, reward, observation_)
+            deep_q.save_transition(observation, action, reward, 0 if done else 1, observation_)
 
-            if (step > 200) and (step % 5 == 0):
+            if step > 200:
                 loss = deep_q.learn()
-                if step % 100 == 0:
+                if step % 50 == 0:
                     print("loss:{}, epsilon:{}, mem_offset:{}"
                           .format(loss,
                                   deep_q.epsilon,
@@ -126,5 +131,5 @@ def run_maze():
     env.destroy()
 
 
-env.after(100, run_maze)
+env.after(1000, run_maze)
 env.mainloop()
